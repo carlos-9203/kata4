e5694d3e8c46f5536bacab70fbd9f082055248c5
Carlos <carlosalonso9797@gmail.com>
2024-01-21 12:39:59 +0000

    creado el main y acabado

--- src/main/java/software/ulpgc/kata4/Main.java
+++ src/main/java/software/ulpgc/kata4/Main.java
@@ -4,14 +4,14 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.List;
-
+//kata 4 con base de datos de videojuegos
 public class Main {
     public static void main(String[] args) throws SQLException {
         try (Connection connection = DriverManager.getConnection(urlOf("dato.db"))){
             TrackLoader trackLoader = new SqliteTrackLoader(connection);
             List<Track> tracks = trackLoader.loadAll();
             for (Track track : tracks){
-                System.out.println(tracks);
+                System.out.println(track);
             }
         }
     }
