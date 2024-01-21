f2f36cbbde530ac6c084d0f4bdef19a2aa58e5a3
Carlos <carlosalonso9797@gmail.com>
2024-01-21 12:32:39 +0000

    creado el Track

---
+++ src/main/java/software/ulpgc/kata4/Track.java
@@ -0,0 +1,5 @@
+package software.ulpgc.kata4;
+
+public record Track(String Platform,String Genre, String Publisher, double NA_Sales, double EU_Sales, double JP_Sales, double Other_Sales, double Global_Sales, String rating, String Critic_Score_Class) {
+
+}
